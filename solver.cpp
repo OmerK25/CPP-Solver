7cb648ad39fc0758cfe033de6604848d5a64d8a8
Omer <omer2katz@gmail.com>
2020-04-27 19:59:32 +0300

    some Real operators

--- solver.cpp
+++ solver.cpp
@@ -2,13 +2,89 @@
 using namespace std;
 using namespace solver;
 
-complex<double> solve(ComplexVariable bi){
+//Operator(+)
+
+const RealVariable RealVariable::operator+(const RealVariable &other) const
+{
+    return RealVariable(this->a + other.a, this->b + other.b, this->c + other.c);
+}
+
+const RealVariable RealVariable::operator+(const double &other) const
+{
+    return RealVariable(this->a, this->b, this->c + other);
+}
+
+const RealVariable operator+(const double &d, const RealVariable &r)
+{
+    return RealVariable(r.a(), r.b(), r.c() + d);
+}
+
+//Operator(-)
+
+const RealVariable RealVariable::operator-(const RealVariable &other) const
+{
+    return RealVariable(this->a - other.a, this->b - other.b, this->c - other.c);
+}
+
+const RealVariable RealVariable::operator-(const double &other) const
+{
+    return RealVariable(this->a, this->b, this->c - other);
+}
+
+const RealVariable operator-(const double &d, const RealVariable &r)
+{
+    return RealVariable(r.a(), r.b(), r.c() - d);
+}
+
+//Operator(*)
+
+const RealVariable RealVariable::operator*(const RealVariable &other) const
+{
+    return RealVariable(this->a * other.a, this->b * other.b, this->c * other.c);
+}
+const RealVariable RealVariable::operator*(const double &other) const
+{
+    return RealVariable(this->a, this->b, this->c * other);
+}
+
+const RealVariable operator*(const double &d, const RealVariable &r)
+{
+    return RealVariable(r.a(), r.b(), r.c() * d);
+}
+
+//Operator(/)
+
+const RealVariable RealVariable::operator/(const double &other) const
+{
+    if (other == 0)
+        throw runtime_error("can't divide by 0, \n Ya Melon");
+
+    return RealVariable(this->a / other, this->b / other, this->c / other);
+}
+
+//operator(^)
+const RealVariable RealVariable::operator^(const double &other) const
+{
+    if (other > 2)
+        throw runtime_error("Al mi ata ba");
+
+    if (other == 1)
+        return *this;
+
+    if (other == 0)
+    {
+        return RealVariable(0, 0, 1);
+    }
+
+    return RealVariable(1, 0, 0);
+}
+
+complex<double> solve(ComplexVariable &bi)
+{
     complex<double> res;
     return res;
 }
-
-
-
-double solve(RealVariable x){
+double solve(RealVariable &x)
+{
     return 0;
 }
--- solver.hpp
+++ solver.hpp
@@ -1,21 +1,74 @@
-#include <complex> 
+#include <complex>
 
 using namespace std;
 
-namespace solver{
+namespace solver
+{
 
-class RealVariable{
-double realVar;
+class RealVariable
+{
+    double a;
+    double b;
+    double c;
 
+public:
+
+    RealVariable(const double &_a=0.0, const double &_b=1.0, const double &_c=0.0) : a(_a), b(_b), c(_c){};
+
+    double a() const
+    {
+        return a;
+    }
+
+    double b() const
+    {
+        return b;
+    }
+    double c() const
+    {
+        return c;
+    }
+
+    const RealVariable operator+(const RealVariable &other) const;
+    const RealVariable operator+(const double &other) const;
+    friend const RealVariable operator+(const double &d, const RealVariable &r);
+
+    const RealVariable operator-(const RealVariable &other) const;
+    const RealVariable operator-(const double &other) const;
+    friend const RealVariable operator-(const double &d, const RealVariable &r);
+
+    const RealVariable operator*(const RealVariable &other) const;
+    const RealVariable operator*(const double &other) const;
+    friend const RealVariable operator*(const double &d, const RealVariable &r);
+
+
+    const RealVariable operator/(const double &other) const;
+  
+
+    const RealVariable operator^(const double &other) const;
 };
 
-class ComplexVariable{
-complex<double> compVar;
+class ComplexVariable
+{
+    // complex<double> compVar;
+    // ComplexVariable(double re, double im)
+    // {
+    //     compVar.real = re;
+    //     compVar.imag = im;
+    // }
 
+    // const ComplexVariable operator+(const ComplexVariable &other) const
+    // {
+    //     return ComplexVariable(other.compVar.real + this->compVar.real, other.compVar.imag + this->compVar.imag);
+    // }
 
+    // const ComplexVariable operator+(const double &other) const
+    // {
+    //     return ComplexVariable(other.compVar.real, other.compVar.imag);
+    // }
 };
 
-double solve(RealVariable x);
-complex<double> solve(ComplexVariable bi);
+double solve(RealVariable &x);
+complex<double> solve(ComplexVariable &bi);
 
-}
+} // namespace solver
